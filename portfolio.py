4bd15902953f7a3d955e3f19e2e4b1b3ff2e30a8
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-01-30 15:42:38 +0100

    added max revenue

--- portfolio.py
+++ portfolio.py
@@ -16,13 +16,19 @@ import scipy.optimize as opt
 import asset as ast
 import security as sqr
 
+# valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,3y,5y,10y,ytd,max
+NDAYS     = 252 # Number of days in the year
+PERIOD    = '1y'
+NUM_PORTS = 10000 # number of portfolios
+#PRECISION = 100 # percent = 100 / per 1000 = 1000, etc
+
 TICKERS = ['MSFT', 'AAPL', 'BRK-B', 'AMZN', 'NFLX', 'XOM']
 Q_S     = [100, 200, 250, 150, 50, 75]
 INDEX   = '^GSPC' # ^GSPC (S&P 500), ^IXIC (Nasdaq), ^DJI (Dow Jones)
 
 #Helper utilities
 def sample_space(returns_df):
-    ''' Explore NUM_PORTS portfolio combinations '''
+    ''' Explore NUM_PORTS portfolio combinations returns_df is log(returns) '''
     ncolumns  = len(returns_df.columns)
     p_weights = np.zeros((NUM_PORTS, ncolumns))
     p_returns = np.zeros(NUM_PORTS)
@@ -51,31 +57,57 @@ def sample_space(returns_df):
     return p_weights, p_returns, p_volat, p_sharpe
 
 
-def display_result(description, opt_value, opt_ret, opt_vol, wts, index):
+def display_result(description, opt_value, p_returns, p_volat, wts, index):
+    opt_ret = p_returns[index]
+    opt_vol = p_volat[index]
+
     print(f'{description} = {opt_value:.4f}')
-    print(f'Location = {index}')
+    print(f'{NUM_PORTS} portfolio combinations')
     print(f'weights: {wts[index,:]}, '
           f'ret={opt_ret:.4f}, '
-          f'vol={opt_vol:.4f}\n')
+          f'vol={opt_vol:.4f}, '
+          f'Sharpe={opt_ret/opt_vol:.4f}')
+
+def display_allocation(description, portfolio, wts, index):
+    print(f"{description} asset allocation ({portfolio.value:.2f} {portfolio.currency}):")
+    for i, weight in enumerate(wts[index,:]):
+        name = portfolio.assets[i].symbol
+        allocation = portfolio.value * weight
+        price = portfolio.assets[i].price
+        opt_q = int(round(allocation/price))
+        print(f'{name}: {allocation:.2f} {portfolio.currency} = '
+              f'{opt_q} * {price:.2f} {portfolio.currency}')
+    print()
 
 
-def plot_rvs(ret, volat, sharpe, max_ret, max_vol, min_ret, min_vol, period):
+def plot_rvs(ret, volat, sharpe, max_i_s, max_i_r, min_i_v, period):
     ''' Plot distribution '''
     SHAPE_SZ    = 75
     SHAPE_COLOR = 'red'
 
-    plt.figure(figsize=(12,8))
-    # all portfolios
-    plt.scatter(volat, ret, c=sharpe, cmap='viridis')
+    shr_ret = ret[max_i_s] # Sharpe
+    shr_vol = volat[max_i_s]
+    max_ret = ret[max_i_r] # Revenue
+    max_vol = volat[max_i_r]
+    min_ret = ret[min_i_v] # Volatility
+    min_vol = volat[min_i_v]
 
-    plt.colorbar(label='Sharpe Ratio')
-    plt.xlabel('Volatility')
-    plt.ylabel('Return')
-    plt.title(f'Period = {period}')
-    # max sharpe
-    plt.scatter(max_vol, max_ret, c=SHAPE_COLOR, s=SHAPE_SZ, marker='o') # red dot
-    # min volatility
+    plt.figure(figsize=(12,8))
+
+    # plot all portfolios
+    plt.scatter(volat, ret, c=sharpe, cmap='viridis')
+    # plot max sharpe portfolio
+    plt.scatter(shr_vol, shr_ret, c=SHAPE_COLOR, s=SHAPE_SZ, marker='o') # red dot
+    # plot min volatility portfolio
     plt.scatter(min_vol, min_ret, c=SHAPE_COLOR, s=SHAPE_SZ, marker='s') # red square
+    # plot max revenue portfolio
+    plt.scatter(max_vol, max_ret, c=SHAPE_COLOR, s=SHAPE_SZ, marker='d') # red diamond
+
+    plt.colorbar(label = 'Sharpe Ratio')
+    plt.xlabel('Volatility')
+    plt.ylabel('Return (log)')
+    plt.title(f'{NUM_PORTS} portfolios ({period})')
+
     plt.show()
 
 
@@ -157,54 +189,39 @@ class Portfolio():
 
     def efficient_frontier(self):
         ''' WIP '''
+        # Sample portfolio space. Returns weights, returns, volatility & sharpe ratios
         all_weights, ret_arr, vol_arr, sharpe_arr = sample_space(self.log_ret)
 
-        max_i      = sharpe_arr.argmax()
-        max_sr_vol = vol_arr[max_i]
-        max_sr_ret = ret_arr[max_i]
+        max_s      = sharpe_arr.argmax() # index of maximum Sharpe ratio
+        max_r      = ret_arr.argmax() # index of maximum revenue
+        min_v      = vol_arr.argmin() # index of minimum volatility
 
-        min_i      = vol_arr.argmin()
-        min_sr_vol = vol_arr[min_i]
-        min_sr_ret = ret_arr[min_i]
-        print('\n\n')
-        display_result('Max Sharpe ratio', sharpe_arr.max(), max_sr_ret, max_sr_vol, all_weights, max_i)
-        display_result('Min volatiliy', vol_arr.min(), min_sr_ret, min_sr_vol, all_weights, min_i)
+        print('\n')
+        display_result('Max Sharpe ratio', sharpe_arr.max(), ret_arr, vol_arr, all_weights, max_s)
+        display_allocation('Max Sharpe', self, all_weights, max_s)
 
+        display_result('Min volatiliy', vol_arr.min(), ret_arr, vol_arr, all_weights, min_v)
+        display_allocation('Min volatility', self, all_weights, min_v)
 
-        print(f"Max Sharpe asset allocation ({self.value:.2f} {self.currency}):")
-        for i, weight in enumerate(all_weights[max_i,:]):
-            name = self.assets[i].symbol
-            allocation = self.value * weight
-            price = self.assets[i].price
-            opt_q = int(round(allocation/price))
-            print(f'{name}: {allocation:.2f} {self.currency} = '
-                  f'{opt_q} * {price:.2f} {self.currency}')
+        display_result('Max revenue', ret_arr.max(), ret_arr, vol_arr, all_weights, max_r)
+        display_allocation('Max revenue', self, all_weights, min_v)
 
-        print(f"Min volatility asset allocation ({self.value:.2f} {self.currency}):")
-        for i, weight in enumerate(all_weights[min_i,:]):
-            name = self.assets[i].symbol
-            allocation = self.value * weight
-            price = self.assets[i].price
-            opt_q = int(round(allocation/price))
-            print(f'{name}: {allocation:.2f} {self.currency} = '
-                  f'{opt_q} * {price:.2f} {self.currency}')
+        plot_rvs(ret_arr, vol_arr, sharpe_arr, max_s, max_r, min_v, self.period)
 
-        plot_rvs(ret_arr, vol_arr, sharpe_arr, max_sr_ret, max_sr_vol, min_sr_ret, min_sr_vol, self.period)
+        # constraints = ({'type':'eq', 'fun':check_sum})
+        # bounds = list()
+        # for asset in self.assets:
+        #     bounds.append((0, 1))
 
-        constraints = ({'type':'eq', 'fun':check_sum})
-        bounds = list()
-        for asset in self.assets:
-            bounds.append((0, 1))
+        # init_guess = list()
+        # for asset in self.assets:
+        #     init_guess.append(1./len(self.assets))
 
-        init_guess = list()
-        for asset in self.assets:
-            init_guess.append(1./len(self.assets))
-
-        opt_results = opt.minimize(neg_sharpe,
-                                    init_guess,
-                                    method='SLSQP',
-                                    bounds=bounds,
-                                    constraints=constraints)
+        # opt_results = opt.minimize(neg_sharpe,
+        #                             init_guess,
+        #                             method='SLSQP',
+        #                             bounds=bounds,
+        #                             constraints=constraints)
 
         # print(get_rvs(opt_results.x))
 
@@ -239,12 +256,6 @@ if __name__ == '__main__':
     start_time = time.time()
     np.random.seed(42)
 
-    PERIOD    = '1y'
-    NDAYS     = 252 # Number of days in the year
-    NUM_PORTS = 1000000 # number of portfolios
-    PRECISION = 100 # percent = 100 / per 1000 = 1000, etc
-
-    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,3y,5y,10y,ytd,max
     assets = list()
 
     for q, ticker in enumerate(TICKERS):
