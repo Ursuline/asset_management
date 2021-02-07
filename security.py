5c0fb13b028b1bcc352bbe7c8c92e89b1e1e5b30
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-02-07 19:09:02 +0100

    pushed Security object floating variables to dictionary

--- security.py
+++ security.py
@@ -6,6 +6,8 @@ Created on Wed Jan 27 20:50:46 2021
 Security object encapsulates yfinance download and provides
 convenience shortcuts
 
+valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+
 @author: charles mégnin
 """
 import yfinance as yf
@@ -30,34 +32,43 @@ class Security():
     ''' A Security is an object resulting from Yahoo finance download using yfinance
         - Provides simplified access to several variables
     '''
-    # pylint: disable=too-many-instance-attributes
 
     def __init__(self, symbol, period):
         print(f'Loading ticker {symbol}')
-        self.symbol   = symbol
-        self.period   = period
+        self.data = {}
+        self.data['symbol'] = symbol
+        self.data['period'] = period
+        #self.symbol   = symbol
+        #self.period   = period
         try:
-            self.ticker   = yf.Ticker(self.symbol)
+            self.ticker   = yf.Ticker(self.data['symbol'])
         except:
             print(f"Couldn't load {symbol}")
         else:
-            self.name     = self.ticker.info['shortName']
-            self.currency = self.ticker.info['currency']
-            self.type     = self.ticker.info['quoteType']
+            # self.name     = self.ticker.info['shortName']
+            # self.currency = self.ticker.info['currency']
+            # self.type     = self.ticker.info['quoteType']
+            self.data['name']     = self.ticker.info['shortName']
+            self.data['currency'] = self.ticker.info['currency']
+            self.data['type']     = self.ticker.info['quoteType']
+
             self._extract_history()
 
 
     def _extract_history(self):
         ''' Makes date index a regular column & extracts Close '''
-        df = self.ticker.history(self.period)
-        df.reset_index(level=0, inplace=True)
-        self.close = df[['Date', 'Close']].copy(deep=True)
-        self.close = self.close.rename(columns = {'Close':'Close_' + self.symbol})
+        df_hist = self.ticker.history(self.data['period'])
+        df_hist.reset_index(level=0, inplace = True)
+        self.close = df_hist[['Date', 'Close']].copy(deep = True)
+        self.close = self.close.rename(columns = {'Close':'Close_' + self.data['symbol']})
 
 
     def describe(self):
         ''' Short Security description '''
-        print(f'{self.name} ({self.symbol}) {self.currency} {self.type} ')
+        print(f'{self.data["name"]} '
+              f'({self.data["symbol"]}) '
+              f'{self.data["currency"]} '
+              f'{self.data["type"]}')
 
 
     def display_details(self):
@@ -68,15 +79,14 @@ class Security():
 
     def display_close(self):
         '''print history for the given period'''
-        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
         print(self.close)
 
 
 #### Driver ####
 if __name__ == '__main__':
-    TICKER = 'PEU.HA'
-    period = '1y'
-    security = Security(TICKER, period)
+    TICKER = 'SPIE.PA'
+    PERIOD = 'max'
+    security = Security(TICKER, PERIOD)
     security.display_details()
     security.describe()
     security.display_close()
