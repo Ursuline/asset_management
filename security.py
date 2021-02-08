6365e3dd04ccd55b4717342119f2a46ae4755e82
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-02-08 17:46:40 +0100

    changed Close to Adj Close to account for splits & dividends

--- security.py
+++ security.py
@@ -5,70 +5,100 @@ Created on Wed Jan 27 20:50:46 2021
 
 Security object encapsulates yfinance download and provides
 convenience shortcuts
+https://pypi.org/project/yfinance/
+https://github.com/ranaroussi/yfinance
+https://aroussi.com/post/python-yahoo-finance
 
 valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
 
 @author: charles mégnin
 """
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 import yfinance as yf
+import equity as eq
 
-### Helper utilities ###
-def annualized_return(rate, ndays):
-    ''' Returns annnualized daily return '''
-    return ndays * (pow(1. + rate/ndays, ndays) - 1.)
+def get_start(period):
+    if period == 'max':
+        td = relativedelta(years=20)
+    elif period[-1] == 'y':
+        td = relativedelta(years=int(period[:-1]))
+    elif period[-1] == 'd':
+        td = dt.timedelta(days=int(period[:-1]))
+    elif period[-1] == 'o' and period[-2] == 'm':
+        td = relativedelta(months=int(period[:-2]))
+    else:
+        raise ValueError(f'invalid period {period}')
+
+    # if period == '1d':
+    #     td = dt.timedelta(days=1)
+    # elif period == '5d':
+    #     td = dt.timedelta(days=5)
+    # elif period == '1mo':
+    #     td = dt.timedelta(months=1)
+    # elif period == '3mo':
+    #     td = dt.timedelta(months=3)
+    # elif period == '6mo':
+    #     td = dt.timedelta(months=6)
+    # elif period == '1y':
+    #     td = relativedelta(years=1)
+    # elif period == '2y':
+    #     td = relativedelta(years=2)
+    # elif period == '3y':
+    #     td = relativedelta(years=3)
+    # elif period == '5y':
+    #     td = relativedelta(years=5)
+    # elif period == '10y':
+    #     td = relativedelta(years=10)
+    # elif period == 'max':
+    #     td = relativedelta(years=20)
+    # else:
+    #     raise ValueError(f"period {period} to be coded")
+
+    return dt.datetime.now() - td
 
 
-def relative_change(df_column):
-    ''' computes relative change bw one column value & the next. First value is NaN'''
-    return (df_column - df_column.shift(1))/df_column.shift(1)
 
-
-def absolute_change(df_column):
-    ''' computes difference bw one column value & the next. First value is NaN'''
-    return df_column - df_column.shift(1)
-
-
-class Security():
+class Security(eq.Equity):
     ''' A Security is an object resulting from Yahoo finance download using yfinance
         - Provides simplified access to several variables
     '''
 
     def __init__(self, symbol, period):
         print(f'Loading ticker {symbol}')
-        self.data = {}
+        super().__init__()
         self.data['symbol'] = symbol
         self.data['period'] = period
-        #self.symbol   = symbol
-        #self.period   = period
         try:
             self.ticker   = yf.Ticker(self.data['symbol'])
         except:
             print(f"Couldn't load {symbol}")
         else:
-            # self.name     = self.ticker.info['shortName']
-            # self.currency = self.ticker.info['currency']
-            # self.type     = self.ticker.info['quoteType']
+            self.history          = yf.download(symbol, get_start(period), dt.datetime.now(), interval='1d')
             self.data['name']     = self.ticker.info['shortName']
             self.data['currency'] = self.ticker.info['currency']
             self.data['type']     = self.ticker.info['quoteType']
+            self.dividends        = self.ticker.dividends
+            self.splits           = self.ticker.splits
+            #self._get_close()
+            self._get_adj_close()
 
-            self._extract_history()
+
+    # def _get_close(self):
+    #     ''' Makes date index a regular column & extracts  Close '''
+    #     df_hist = self.ticker.history(self.data['period'])
+    #     df_hist.reset_index(level=0, inplace = True)
+    #     self.close = df_hist[['Date', 'Close']].copy(deep = True)
+    #     self.close = self.close.rename(columns = {'Close':'Close_' + self.data['symbol']})
 
 
-    def _extract_history(self):
-        ''' Makes date index a regular column & extracts Close '''
-        df_hist = self.ticker.history(self.data['period'])
+    def _get_adj_close(self):
+        ''' Makes date index a regular column & extracts Adjusted Close '''
+
+        df_hist = self.history
         df_hist.reset_index(level=0, inplace = True)
-        self.close = df_hist[['Date', 'Close']].copy(deep = True)
-        self.close = self.close.rename(columns = {'Close':'Close_' + self.data['symbol']})
-
-
-    def describe(self):
-        ''' Short Security description '''
-        print(f'{self.data["name"]} '
-              f'({self.data["symbol"]}) '
-              f'{self.data["currency"]} '
-              f'{self.data["type"]}')
+        self.close = df_hist[['Date', 'Adj Close']].copy(deep = True)
+        self.close = self.close.rename(columns = {'Adj Close':'Close_' + self.data['symbol']})
 
 
     def display_details(self):
@@ -82,11 +112,11 @@ class Security():
         print(self.close)
 
 
-#### Driver ####
+#### Test Driver ####
 if __name__ == '__main__':
-    TICKER = 'SPIE.PA'
-    PERIOD = 'max'
+    TICKER   = 'MSFT'
+    PERIOD   = '6mo'
     security = Security(TICKER, PERIOD)
     security.display_details()
-    security.describe()
     security.display_close()
+    # security.describe()
