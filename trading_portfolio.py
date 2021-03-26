0ce8dbff66df2beb17ef404c4b57f43cddd8193e
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-03-26 23:15:36 +0100

    initial commit

---
+++ trading_portfolio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 23:12:46 2021
+
+trading_portfolio.py
+
+Lists of securities to be used by trading_driver.py
+
+@author: charly
+"""
+
+INDICES    = ['^GSPC', '^DJI', '^IXIC', '^FCHI', '^N225', '^HSI', '^DJSH']
+DEFENSE    = ['HO.PA']
+HEALTHCARE = ['TDOC', 'BLC.PA', 'PHA.PA', 'KORI.PA', 'ORP.PA']
+FRENCH     = ['SU.PA', 'MC.PA', 'RMS.PA', 'FP.PA', 'STLA.PA']
+K_WOOD     = ['ARKF', 'ARKG', 'ARKK', 'ARKQ', 'ARKW']
+TECH       = ['AAPL', 'TSLA', 'AMZN', 'MSFT', 'SPOT', 'SQ', 'SHOP']
+COMM       = ['ROKU', 'ZM', 'BIDU']
+CRYPTO     = ['BTC-USD', 'ETH-USD']
+ADRIEN     = ['ATO.PA', 'BN.PA', 'BON.PA', 'CA.PA', 'GLE.PA', 'HEXA.PA', 'SPIE.PA', 'TRI.PA']
