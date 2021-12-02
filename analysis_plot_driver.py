48ea7e2c69e190e991024c7f0c2333b6157a3d28
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-02 11:09:10 +0100

    stable driver for fundamentals plot

---
+++ analysis_plot_driver.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 09:52:09 2021
+
+Driver for fundamental analysis plot sequence
+
+@author: charles mégnin
+"""
+import os
+import sys
+import pandas as pd
+import plotly.io as pio
+import company as cny
+
+pio.renderers.default = 'browser'
+
+PERIOD   = 'annual'
+PLOT_DIR = 'plots'
+
+# Target company specs
+TICKER          = 'SU.PA'
+EXPIRATION_DATE = '2021-12-31'
+YEAR_0          = int('2013')
+YEAR_1          = int('2020')
+
+
+def get_metrics_over_time(cie, items:list, yr_0:int, yr_1:int):
+    '''Generic getter'''
+    # Get metric
+    df_0 = cie.load_cie_metrics_over_time(items, yr_0, yr_1, change=False)
+    # Get change in metric
+    df_1 = cie.load_cie_metrics_over_time(items, yr_0, yr_1, change=True)
+    return pd.concat([df_0, df_1], axis=1) # merge the two
+
+
+def get_balance_sheet(cie, yr_0:int, yr_1:int):
+    '''
+    Returns a dataframe with assets, liabilities & equity and their relative changes over time
+    '''
+    items = ['totalAssets', 'totalLiabilities', 'totalStockholdersEquity']
+    return get_metrics_over_time(cie, items, yr_0, yr_1)
+
+
+def get_revenue_fcf_ebit(cie, yr_0:int, yr_1:int):
+    '''
+    Returns a dataframe with revenue, free cash flow & EBIT and their relative changes over time
+    '''
+    items = ['revenue', 'ebit', 'freeCashFlow']
+    return get_metrics_over_time(cie, items, yr_0, yr_1)
+
+
+def get_dupont_metrics(cie, yr_0:int, yr_1:int):
+    '''
+    Returns a dataframe with cash ROE, ROE, net profit margin, asset turnover, equity multiplier
+    '''
+    items = ['returnOnEquity', 'netProfitMargin', 'assetTurnover', 'equityMultiplier']
+    return get_metrics_over_time(cie, items, yr_0, yr_1)
+
+
+def get_wb_metrics(cie, yr_0:int, yr_1:int):
+    '''
+    Returns a dataframe with ROE, debt to equity, current ratio
+    '''
+    items = ['returnOnEquity', 'debtToEquity', 'currentRatio']
+    return get_metrics_over_time(cie, items, yr_0, yr_1)
+
+
+if __name__ == '__main__':
+    os.makedirs(PLOT_DIR, exist_ok = True)
+    company = cny.Company(ticker=TICKER, period=PERIOD, expiration_date=EXPIRATION_DATE)
+    root = f'{TICKER}_{YEAR_0}_{YEAR_1}'
+
+    # Build & plot revenue/FCF/EBIT dataframe
+    rfe = get_revenue_fcf_ebit(company, YEAR_0, YEAR_1)
+    path = os.path.join(PLOT_DIR, f'{root}_income.html')
+    company.fundamentals_plot(time_series=rfe,
+                              plot_type = 'revenue',
+                              subtitle='Income & Free cash flow',
+                              filename=path,
+                              )
+
+    # Build & plot balance sheet dataframe
+    bs = get_balance_sheet(company, YEAR_0, YEAR_1)
+    path = os.path.join(PLOT_DIR, f'{root}_bs.html')
+    company.fundamentals_plot(time_series=bs,
+                              plot_type = 'bs',
+                              subtitle='Balance sheet',
+                              filename=path,
+                              )
+
+    # Build & plot Dupont metrics dataframe
+    dupont = get_dupont_metrics(company, YEAR_0, YEAR_1)
+    path = os.path.join(PLOT_DIR, f'{root}_dupont.html')
+    company.fundamentals_plot(time_series=dupont,
+                              plot_type = 'dupont',
+                              subtitle='Dupont metrics',
+                              filename=path,
+                              )
+
+    # Build & plot Warren Buffet metrics dataframe
+    wb_metrics = get_wb_metrics(company, YEAR_0, YEAR_1)
+    path = os.path.join(PLOT_DIR, f'{root}_wb.html')
+    company.fundamentals_plot(time_series=wb_metrics,
+                              plot_type = 'wb',
+                              subtitle='"Warren Buffet" metrics',
+                              filename=path,
+                              )
