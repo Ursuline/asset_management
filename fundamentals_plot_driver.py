db5a6503c1acfa3f0c2c095b1afa1bc32e512c21
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-09 16:23:30 +0100

    refactored

--- fundamentals_plot_driver.py
+++ fundamentals_plot_driver.py
@@ -33,56 +33,13 @@ if __name__ == '__main__':
     prefix = f'{TICKER}_{YEAR_0}-{YEAR_1}_{EXPIRATION_DATE}'
     cie = cny.Company(ticker=TICKER, period='annual', expiration_date=EXPIRATION_DATE)
 
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('bs_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'bs',
-                          subtitle    = mtr.metrics_set_names['bs_metrics'],
-                          filename    = os.path.join(DIR, prefix + '_bs.html'),
-                          )
-
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('income_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'income',
-                          subtitle    = mtr.metrics_set_names['income_metrics'],
-                          filename    = os.path.join(DIR, prefix + '_bs.html'),
-                          )
-
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('wb_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'wb',
-                          subtitle    = mtr.metrics_set_names['wb_metrics'],
-                          filename    = os.path.join(DIR, prefix + '_wb.html'),
-                          )
-
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('dupont_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'dupont',
-                          subtitle    = mtr.metrics_set_names['dupont_metrics'],
-                          filename    = os.path.join(DIR, prefix + '_dupont.html'),
-                          )
-
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('valuation_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'valuation',
-                          subtitle    = 'Valuation metrics',
-                          filename    = os.path.join(DIR, prefix + '_valuation.html'),
-                          )
-
-    cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics('dividend_metrics'),
-                                                          yr_0    = YEAR_0,
-                                                          yr_1    = YEAR_1,
-                                                          ),
-                          plot_type   = 'dividend',
-                          subtitle    = mtr.metrics_set_names['dividend_metrics'],
-                          filename    = os.path.join(DIR, prefix + '_dividend.html'),
-                          )
+    plot_types = ['bs', 'income', 'wb', 'dupont', 'valuation', 'dividend']
+    for plot_type in plot_types:
+        cie.fundamentals_plot(time_series = aggregate_metrics(metrics = mtr.get_metrics(f'{plot_type}_metrics'),
+                                                              yr_0    = YEAR_0,
+                                                              yr_1    = YEAR_1,
+                                                              ),
+                              plot_type   = plot_type,
+                              subtitle    = mtr.metrics_set_names[f'{plot_type}_metrics'],
+                              filename    = os.path.join(DIR, prefix + f'_{plot_type}.html'),
+                              )
