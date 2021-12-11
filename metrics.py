168a0c517d6c1a23843156237fc2a821e12595ba
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-11 18:47:49 +0100

    added income #2

--- metrics.py
+++ metrics.py
@@ -7,6 +7,20 @@ metrics.py - metrics/ratio & values
 
 ratios & related-utilities
 
+Dupont metrics split cash return on equity (CROE) into:
+net profit margin * asset turnover * equity multiplier * cash conversion
+        |__________________|
+                ROA
+                |____________________________|
+                                ROE
+                                |______________________________|
+                                                CROE
+net profit margin = net income/sales -> profitability
+asset turnover = sales/mean asset ->` efficiency of management
+equity multiplier = asset/equity -> leverage
+cash conversion -> Free cash flow / Net income
+roe = NI / Equity & croe = FCF / Equity
+
 @author: charly
 """
 import inspect
@@ -17,30 +31,48 @@ metrics_set_names = {'mktcap_metrics': 'Assets, Revenue & Market Cap',
                      'dupont_metrics': 'Dupont metrics',
                      'bs_metrics': 'Balance sheet metrics',
                      'income_metrics': 'Income & Free cash flow metrics',
+                     'income2_metrics': 'Income & Free cash flow metrics #2',
                      'valuation_metrics': 'Valuation metrics',
                      'valuation2_metrics': 'Valuation metrics #2',
                      'dividend_metrics': 'Dividend metrics',
+                     'debt_metrics': 'Debt metrics',
                      }
 
-wb_metrics  = {'returnOnEquity':.08, 'debtToEquity':0.5, 'currentRatio':1.5}
-valuation_metrics = {'priceToBookRatio':0, 'peg':0}
-valuation2_metrics = {'peRatio':0, 'evToebit':0, 'priceToSalesRatio':0}
-dividend_metrics = {'dividendYield':0, 'payoutRatio':0}
+mktcap_metrics = {'totalAssets': 0., 'revenue': 0., 'mktCap': 0.,}
+bs_metrics = {'totalAssets': 0., 'totalLiabilities': 0., 'totalStockholdersEquity': 0.,}
+income_metrics = {'revenue': 0., 'ebit': 0., 'freeCashFlow': 0.,}
+income2_metrics = {'grossProfitRatio': 0., 'ebitPerRevenue': 0., 'freeCashFlowToRevenue': 0.,}
+wb_metrics  = {'returnOnEquity':.08, 'debtToEquity':0.5, 'currentRatio':1.5,}
+valuation_metrics = {'priceToBookRatio':0, 'peg':0,}
+valuation2_metrics = {'peRatio':0, 'evToebit':0, 'priceToSalesRatio':0,}
+dividend_metrics = {'dividendYield':0, 'payoutRatio':0,}
+dupont_metrics = {'returnOnEquity':.08, 'netProfitMargin':0, 'assetTurnover':0, 'equityMultiplier':0,}
+full_dupont_metrics = {'returnOnEquity':.08, 'netProfitMargin':0, 'assetTurnover':0, 'equityMultiplier':0, \
+                       'cashConv':0, 'roa':0, 'cashReturnOnEquity':0,}
+debt_metrics = {'debtToEquity':0.5, 'debtToAssets':0, 'interestCoverage':0, \
+                'shortTermCoverageRatios':0,}
 
-captions = {'ROE':               'ROE: Net income / Total shareholder equity',
-            'assetTurnover':     'Asset turnover: Sales / Mean total assets',
+captions = {'assetTurnover':     'Asset turnover: Sales / Mean total assets',
             'cashConversion':    'Cash conversion: Free cash flow / Net income',
             'currentRatio':      'Current ratio: Total current assets / Total current liabilities',
-            'debtToEquity':      'Debt to equity ratio: Long-term debt / Total shareholder equity',
-            'dividendYield':     'Dividend yield: Dividend paid / (Price x # of shares)',
+            'debtToEquity':      'Debt-to-equity ratio: LT debt / Total shareholder equity',
+            'debtToAssets':      'Debt-to-assets ratio: LT debt / Total assets',
+            'dividendYield':     'Dividend yield: Dividend paid / Market cap',
+            'ebitPerRevenue':    'Ebit-to-sales: ebit / Revenue',
             'equityMultiplier':  'Equity multiplier: Total assets / Total equity',
-            'evToebit':          'E.V. to ebit: Enterprise value / ebit',
+            'evToebit':          'E.V.-to-ebit: Enterprise value / ebit',
+            'freeCashFlowToRevenue': 'FCF-to-revenue: FCF / Revenue',
+            'grossProfitRatio':  'Gross margin: Gross profit / Revenue',
+            'interestCoverage':  'Interest coverage: Interest expense / ebit',
+            'netDebtToebit':     'Net debt-to-ebit: (Total debt - Cash & Cash equivalents) / ebit',
             'netProfitMargin':   'Net profit margin: Net income / Sales',
             'payoutRatio':       'Payout ratio: Dividend paid / Net income',
-            'peg':               'P/E to growth: P/E ratio / expected growth',
-            'peRatio':           'P/E ratio: Price x # of shares / Net income',
-            'priceToBookRatio':  'P/B: Price x # of shares / Total shareholder equity',
-            'priceToSalesRatio': 'P/B: Price x # of shares / Revenue',
+            'peg':               'P/E-to-growth: P/E ratio / expected growth',
+            'peRatio':           'P/E ratio: Market cap / Net income',
+            'priceToBookRatio':  'P/B: Market cap / Total shareholder equity',
+            'priceToSalesRatio': 'P/B: Market cap / Revenue',
+            'shortTermCoverageRatios': 'ST coverage ratio: ST debt / Operationg cash flow',
+            'ROE':               'ROE: Net income / Total shareholder equity',
             }
 
 metrics_captions = {'dupont'    : f'{captions["ROE"]} | {captions["netProfitMargin"]} | \
@@ -48,30 +80,11 @@ metrics_captions = {'dupont'    : f'{captions["ROE"]} | {captions["netProfitMarg
                     'wb'        : f'{captions["ROE"]} | {captions["debtToEquity"]} | {captions["currentRatio"]}',
                     'valuation' : f'{captions["priceToBookRatio"]} | {captions["peg"]}',
                     'valuation2': f'{captions["peRatio"]} | {captions["evToebit"]}| {captions["priceToSalesRatio"]}',
+                    'income2'   : f'{captions["grossProfitRatio"]} | {captions["ebitPerRevenue"]}| {captions["freeCashFlowToRevenue"]}',
                     'dividend'  : f'{captions["dividendYield"]} | {captions["payoutRatio"]}',
+                    'debt'      : f'{captions["debtToEquity"]} | {captions["debtToAssets"]} | \
+                    {captions["netDebtToebit"]} | {captions["interestCoverage"]} | {captions["shortTermCoverageRatios"]}',
                     }
-
-mktcap_metrics = {'totalAssets': 0., 'revenue': 0., 'mktCap': 0.}
-bs_metrics = {'totalAssets': 0., 'totalLiabilities': 0., 'totalStockholdersEquity': 0.}
-income_metrics = {'revenue': 0., 'ebit': 0., 'freeCashFlow': 0.}
-
-
-# Dupont metrics split cash return on equity (CROE) into:
-# net profit margin * asset turnover * equity multiplier * cash conversion
-#        |__________________|
-#                ROA
-#                 |____________________________|
-#                                ROE
-#                                 |______________________________|
-#                                                CROE
-# net profit margin = net income/sales -> profitability
-# asset turnover = sales/mean asset ->` efficiency of management
-# equity multiplier = asset/equity -> leverage
-# cash conversion -> Free cash flow / Net income
-# roe = NI / Equity & croe = FCF / Equity
-
-dupont_metrics = {'returnOnEquity':.08, 'netProfitMargin':0, 'assetTurnover':0, 'equityMultiplier':0}
-full_dupont_metrics = {'returnOnEquity':.08, 'netProfitMargin':0, 'assetTurnover':0, 'equityMultiplier':0, 'cashConv':0, 'roa':0, 'cashReturnOnEquity':0}
 
 def get_metrics(group:str, metric:str=None):
     '''returns metric value group=dictionary name / metric=metric kind.
@@ -97,6 +110,10 @@ def get_metrics(group:str, metric:str=None):
             if metric is None:
                 return income_metrics #return dictionary
             return income_metrics[metric] # return value
+        if group == 'income2_metrics':
+            if metric is None:
+                return income2_metrics #return dictionary
+            return income2_metrics[metric] # return value
         if group == 'income_metrics':
             if metric is None:
                 return income_metrics #return dictionary
@@ -113,6 +130,10 @@ def get_metrics(group:str, metric:str=None):
             if metric is None:
                 return dividend_metrics #return dictionary
             return dividend_metrics[metric] # return value
+        if group == 'debt_metrics':
+            if metric is None:
+                return debt_metrics #return dictionary
+            return debt_metrics[metric] # return value
 
         caller_name = inspect.stack()[1][3]
         func_name   = inspect.stack()[0][3]
