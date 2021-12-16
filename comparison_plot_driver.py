0bfeb797c23a1ce4acb085a1ce08830b7e4e01c9
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-16 15:57:05 +0100

    preparing driver for switch to bokeh

--- comparison_plot_driver.py
+++ comparison_plot_driver.py
@@ -32,48 +32,41 @@ XCHANGE  = False
 # Window of mktCap to include as a fraction of target company mktCap
 MKT_CAP_WINDOW = {'lower': .1,
                   'upper': 10}
+EXPIRATION_DATE = '2021-12-31'
 
 # Target company specs
-TARGET_TICKER   = 'SU.PA'
-START_DATE      = '2010-01-02'
-END_DATE        = '2021-11-01'
-EXPIRATION_DATE = '2021-12-31'
+TARGET_TICKER   = 'MC.PA'
 YEAR            = '2019'
 
 if __name__ == '__main__':
-    YEAR = str(YEAR)
-
     stocks = pd.read_csv(DATA_PATH)
     target_stock = stocks[stocks.symbol == TARGET_TICKER]
-    cie = cny.Company(TARGET_TICKER,
+    cie = cny.Company(ticker          = TARGET_TICKER,
                       period          = PERIOD,
                       expiration_date = EXPIRATION_DATE,
-                      start_date      = START_DATE,
-                      end_date        = END_DATE,
                       )
-
-    filter_d = {
-                'industry':          (target_stock['industry'].values[0], INDUSTRY),
+    filter_d = {'industry':          (target_stock['industry'].values[0], INDUSTRY),
                 'sector':            (target_stock['sector'].values[0], SECTOR),
                 'mktCapUSD':         (target_stock['mktCapUSD'].values[0], MKTCAP),
                 'mktCap_interval':   (MKT_CAP_WINDOW['lower'],
-                                      MKT_CAP_WINDOW['upper']),
+                                      MKT_CAP_WINDOW['upper'],
+                                      ),
                 'currency':          (target_stock['currency'].values[0], CURRENCY),
                 'country':           (target_stock['country'].values[0], COUNTRY),
                 'exchangeShortName': (target_stock['exchangeShortName'].values[0], XCHANGE),
                 }
-
-    util.filter_to_screen(filter_d)
+    util.filter_to_screen(filt=filter_d,
+                          currency=cie.get_currency_symbol(),
+                          )
 
     peers = util.extract_peers(target_ticker=TARGET_TICKER, filt=filter_d)
-    # If it exists, remove target stock
-    peers.discard(TARGET_TICKER)
+    peers.discard(TARGET_TICKER) # If it exists, remove target stock to avoid duplicate
     print(f'{len(peers)} peers returned\n')
     print(peers)
     prefix = f'{TARGET_TICKER}_{YEAR}_'
     for metric in mtr.metrics_set_names:
         filename = prefix + f'{metric}_comparison.html'
-        cie.plot_peers_comparison(year        = YEAR,
+        cie.plot_peers_comparison(year        = int(YEAR),
                                   metrics     = mtr.get_metrics(metric),
                                   metric_name = metric,
                                   peers       = peers,
