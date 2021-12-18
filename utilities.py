e163d49c0d67a9e1c9792769df3cf317276cb5d4
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-18 10:32:07 +0100

    moved methods in & out

--- utilities.py
+++ utilities.py
@@ -5,6 +5,7 @@ Created on Mon Oct 25 17:03:05 2021
 
 @author: charly
 """
+import math
 import json
 import pandas as pd
 import requests
@@ -14,7 +15,6 @@ URL  = 'https://ml-finance.ams3.digitaloceanspaces.com'
 PATH = 'fundamentals'
 FILE = 'stocks.csv'
 DATA_PATH  = f'{URL}/{PATH}/{FILE}'
-MILLION = 1000000.
 
 def build_url(ticker:str, api_key:str):
     '''Builds the FMP URL for the given ticker'''
@@ -42,42 +42,8 @@ def extract_peers_fmp(ticker:str, api_key:str):
         raise ValueError('Badly formed URL {url}')
 
 
-def extract_peers(target_ticker:str, filt:dict):
-    '''
-    Extracts list of peers. Expects:
-    target ticker
-    filter: a dictionary of the form:
-    filter = {
-        'industry': (target_stock['industry'].values[0], True),
-        'sector': (target_stock['sector'].values[0], True),
-        'mktCapUSD': (target_stock['mktCapUSD'].values[0], True),
-        'mktCap_interval':(mkt_cap_lower, mkt_cap_upper),
-        'currency': (target_stock['currency'].values[0], False),
-        'country': (target_stock['country'].values[0], False),
-        'exchangeShortName': (target_stock['exchangeShortName'].values[0], False),
-        }
-    where True/False is the flag that determines whether or not to apply
-    the filter
-    mkt_cap_lower, mkt_cap_upper are upper and lower bounds of market cap
-    expressed as fractions of the target market cap
-    '''
-    stocks = pd.read_csv(DATA_PATH)
-    target_stock = stocks[stocks.symbol == target_ticker]
-
-    #Returns list of companies matching filter values tagged as True'''
-    for key in list(filt.keys()):
-        if filt[key][1] is True:
-            if key == 'mktCapUSD':
-                lower = target_stock[key].values[0] * filt['mktCap_interval'][0]
-                upper = target_stock[key].values[0] * filt['mktCap_interval'][1]
-                stocks = stocks[(stocks[key] >= lower) & (stocks[key] <= upper)]
-            else: # not market cap
-                stocks = stocks[stocks[key] == filt[key][0]]
-    return set(stocks.symbol)
-
-
-def filter_to_screen(filt:dict, currency:str):
-    '''Outputs company filter characterisitcs to screen'''
+def echo_filter(filt:dict, currency:str):
+    '''Outputs company filter characteristics to screen'''
     for key in filt:
         if filt[key][1] == True:
             status = 'on'
@@ -95,3 +61,14 @@ def filter_to_screen(filt:dict, currency:str):
         else:
             print(f'{key}: {filt[key][0]} status={status}')
     print()
+
+
+# Math utilities
+def round_up(val:float, ndigits:int):
+    '''round up utility'''
+    return (math.ceil(val * math.pow(10, ndigits)) + 1) / math.pow(10, ndigits)
+
+
+def round_down(val:float, ndigits:int):
+    '''round down utility'''
+    return (math.floor(val * math.pow(10, ndigits)) - 1) / math.pow(10, ndigits)
