efe888eadd74ed3772aab26e810599d7bfc0f841
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2021-12-30 23:11:56 +0200

    added tqdm & tabulated output

--- suggest_peers.py
+++ suggest_peers.py
@@ -5,11 +5,24 @@ Created on Fri Dec 24 17:18:23 2021
 
 suggest_peers.py
 
+Utility to extract peers to a base company
+Set as True at least one optional filter based on:
+    -Industry
+    -Sector
+    -Market cap
+    -Currency
+    -Country
+    -Stock exchange
+
 @author: charles megnin
 """
 import os
 import csv
 import time
+from numerize import numerize
+from tabulate import tabulate
+from tqdm import tqdm
+import yfinance as yf
 import pandas as pd
 import company as cny
 import plotter_defaults as dft
@@ -18,8 +31,8 @@ import utilities as util
 # Filter flags
 INDUSTRY = True
 SECTOR   = True
-MKTCAP   = True
-CURRENCY = False
+MKTCAP   = False
+CURRENCY = True
 COUNTRY  = False
 XCHANGE  = False
 
@@ -29,14 +42,14 @@ MKT_CAP_WINDOW = {'lower': .1,
 EXPIRATION_DATE = '2021-12-31'
 
 # Target company specs
-TARGET_TICKER   = 'RMS.PA'
+TARGET_TICKER   = 'BEN.PA'
 
 
 def extract_peers(target_ticker:str, stocks:pd.DataFrame, filt:dict):
     '''
     Extracts list of peers. Expects:
-    target ticker
-    filter: a dictionary of the form:
+    -target ticker
+    -filter: a dictionary of the form:
     filter = {
         'industry': (target_stock['industry'].values[0], True),
         'sector': (target_stock['sector'].values[0], True),
@@ -49,7 +62,7 @@ def extract_peers(target_ticker:str, stocks:pd.DataFrame, filt:dict):
     where True/False is the flag that determines whether or not to apply
     the filter
     mkt_cap_lower, mkt_cap_upper are upper and lower bounds of market cap
-    expressed as fractions of the target market cap
+    expressed as fractions of the target market cap in US$
     returns a set of tickers
     '''
     target_stock = stocks[stocks.symbol == target_ticker]
@@ -66,6 +79,64 @@ def extract_peers(target_ticker:str, stocks:pd.DataFrame, filt:dict):
     return stocks.symbol
 
 
+def echo_filter(filt:dict, currency:str):
+    '''Outputs company filter characteristics to screen'''
+    for key in filt:
+        if filt[key][1] == True:
+            status = 'on'
+        else:
+            status = 'off'
+        if key == 'mktCapUSD':
+            base = filt[key][0]
+            lower = base * filt['mktCap_interval'][0]
+            upper = base * filt['mktCap_interval'][1]
+            if status == 'on':
+                msg  = f'{key}: {currency}{numerize.numerize(lower)} - {currency}{numerize.numerize(upper)} '
+                msg += f'(base={currency}{numerize.numerize(base)})'
+                print(msg)
+        elif key == 'mktCap_interval':
+            continue
+        else:
+            if status == 'on':
+                print(f'{key.capitalize()}: {filt[key][0]}')
+    print()
+
+
+def echo_peers(peers:list, peer_names:list):
+    '''Print result to screen'''
+    print(tabulate(pd.DataFrame(list(zip(peers,
+                                         peer_names,
+                                         )
+                                     ),
+                                columns=['Ticker', 'Name'],
+                                ),
+                   showindex = True,
+                   headers   = 'keys',
+                   tablefmt  = 'fancy_grid',
+                   )
+          )
+
+
+def get_peer_names_old(peers:list):
+    '''Returns as a list the names of peers in the input ticker list'''
+    names = []
+    for peer in peers:
+        company = cny.Company(ticker          = peer,
+                          period          = dft.PERIOD,
+                          expiration_date = EXPIRATION_DATE,
+                          )
+        names.append(company.get_company_name())
+    return names
+
+
+def get_peer_names(peers:list):
+    names = []
+    for peer in tqdm(peers):
+        cie = yf.Ticker(peer)
+        names.append(cie.info['longName'])
+    return names
+
+
 def save_data(peers:list, file_name:str):
     '''Save list of peers as a 1-column csv file'''
     os.makedirs(dft.get_data_directory(),
@@ -75,7 +146,7 @@ def save_data(peers:list, file_name:str):
     with open(output_file, 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows(zip(peers))
-    print(f'list of suggested peer tickers saved as:\n{output_file}')
+    print(f'\nList of base + suggested peer tickers saved as:\n{output_file}')
 
 
 if __name__ == '__main__':
@@ -97,9 +168,9 @@ if __name__ == '__main__':
                 'country':           (target_security['country'].values[0], COUNTRY),
                 'exchangeShortName': (target_security['exchangeShortName'].values[0], XCHANGE),
                 }
-    util.echo_filter(filt     = filter_d,
-                     currency = cie.get_currency_symbol(),
-                     )
+    echo_filter(filt     = filter_d,
+                currency = cie.get_currency_symbol(),
+                )
     peer_list = extract_peers(target_ticker = TARGET_TICKER,
                               stocks        = securities,
                               filt          = filter_d,
@@ -107,8 +178,11 @@ if __name__ == '__main__':
     #Remove any occurence of target from list
     peer_list = [value for value in peer_list if value != TARGET_TICKER]
     peer_list = list(set(peer_list)) # remove other possible duplicates
+
     # insert target at the beginning of the list
     peer_list.insert(0, TARGET_TICKER)
-    print(f'{len(peer_list)-1} peers returned + {TARGET_TICKER}: {peer_list}')
+    print(f'{len(peer_list)-1} peers returned')
+    echo_peers(peer_list,
+               get_peer_names(peer_list))
     save_data(peer_list, peer_filename)
-    print(f"Total elapsed time: {util.convert_seconds(time.time()-start_tm)}")
+    print(f"\nTotal elapsed time: {util.convert_seconds(time.time()-start_tm)}")
