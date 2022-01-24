9a65fcbeccdc3283547f8ecbf25a324d0aefedea
Charles Mégnin <3534155+Ursuline@users.noreply.github.com>
2022-01-24 11:41:59 +0100

    added exception handlig

--- display_portfolio.py
+++ display_portfolio.py
@@ -5,7 +5,7 @@ Created on Tue Dec 28 09:02:04 2021
 
 Outputs the contents of a portfolio to screen
 Usage:
-pythobn display_portfolio.py [portfolio_name]
+python display_portfolio.py [portfolio file name with .csv extension]
 
 display_portfolio.py
 
@@ -15,22 +15,34 @@ import os
 import sys
 from tabulate import tabulate
 import pandas as pd
-from charting import parameters_sync as dft
 
+# Cloud server
+SERVER_URL     = 'https://ml-finance.ams3.digitaloceanspaces.com'
+SERVER_PATH    = 'charting/portfolios'
+PORTFOLIO_DIR  = f'{SERVER_URL}/{SERVER_PATH}/'
+RUN = 'local'
 
 if __name__ == '__main__':
 
-    url = os.path.join(dft.PORTFOLIO_DIR,
-                       sys.argv[1]
+    url = os.path.join(PORTFOLIO_DIR,
+                       #sys.argv[1],
+                       'watchlist.csv'
                        )
     try:
         contents = pd.read_csv(url)
-        contents.loc[0] = ['Ticker', 'Position', 'Strategy']
+    except:
+        print(f'Could not load file {sys.argv[1]} from {url}')
+        sys.exit(0)
+    contents.loc[0] = ['Ticker', 'Position', 'Strategy']
+    if RUN == 'local':
         print(tabulate(contents,
                        showindex = True,
                        tablefmt  = 'fancy_grid',
                        )
               )
-    except:
-        print(f'Could not load file {sys.argv[1]} from {url}')
-        sys.exit(0)
+    elif RUN == 'remote':
+        print(contents.to_html())
+    else:
+        msg = f'Value for variable {RUN=} should be "remote" or "local"'
+        raise KeyError(msg)
+
